700c5c8c1f9aed3937af93dc7539b0658fd001b6
evaristocuesta <evaristocg@gmail.com>
2024-03-08 16:06:53 +0100

    Add project files.

---
+++ src/StringConcatSpanBenchmark/BenchmarkStringConcat.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+
+namespace StringConcatSpanBenchmark;
+
+[MemoryDiagnoser(displayGenColumns: true)]
+public class BenchmarkStringConcat
+{
+    private const string _text = "Lorem Ipsum is simply dummy test";
+
+    [Benchmark]
+    public string MethodSubstring()
+    {
+        return _text.Substring(10) + "---" + _text.Substring(0, 5);
+    }
+
+    [Benchmark]
+    public string MethodInterpolateSubstring()
+    {
+        return $"{_text.Substring(10)}---{_text.Substring(0, 5)}";
+    }
+
+    [Benchmark]
+    public string MethodConcatSubstring()
+    {
+        return string.Concat(_text.Substring(10), "---", _text.Substring(0, 5));
+    }
+
+    [Benchmark(Baseline = true)]
+    public string MethodConcatAsSpan()
+    {
+        return string.Concat(_text.AsSpan(10), "---", _text.AsSpan(0, 5));
+    }
+}
---
+++ src/StringConcatSpanBenchmark/Program.cs
@@ -0,0 +1,10 @@
+﻿using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Running;
+using StringConcatSpanBenchmark;
+
+var options = DefaultConfig.Instance.WithSummaryStyle(
+    summaryStyle: SummaryStyle.Default.WithRatioStyle(RatioStyle.Trend));
+
+_ = BenchmarkRunner.Run<BenchmarkStringConcat>(options);
